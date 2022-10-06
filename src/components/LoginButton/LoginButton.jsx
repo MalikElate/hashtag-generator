9ec39f350ce275035d5f76586598471cf7b06084
Malik Elate <Malik.e1955@gmail.com>
2022-10-05 22:02:59 -0500

    changed to href

--- src/components/LoginButton/LoginButton.jsx
+++ src/components/LoginButton/LoginButton.jsx
@@ -8,24 +8,18 @@ function LoginButton() {
     const your_callback_url = 'http://localhost:3000/'; 
     const your_client_id = '869xty0vmoa48p'; 
     const scope = 'r_liteprofile%20r_emailaddress%20w_member_social'; 
-    
+    const signInLink =  "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=869xty0vmoa48p&redirect_uri=http://localhost:3000/&state=foobar&scope=r_liteprofile%20r_emailaddress%20w_member_social"
+            
     const handleInput = (e) => {
         e.preventDefault();
         console.log("hello world") 
-        window.location.href = 
-        `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${your_client_id}&redirect_uri=${your_callback_url}&state=foobar&scope=r_liteprofile%20r_emailaddress%20w_member_social`; 
-        
-   
-        // fetch('https://www.linkedin.com/oauth/v2/authorization?' + new URLSearchParams({
-        //         response_type: 'code',
-        //         client_id: your_client_id,
-        //         redirect_uri:your_callback_url,
-        //         scope: scope
-        //     }))
+
+        window.open(signInLink); 
     }
 
     return (
             <button
+                type="button" 
                 className='login-button'
                 onClick={handleInput}> Generate My Hashtags
             </button>
